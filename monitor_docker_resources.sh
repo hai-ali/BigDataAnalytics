abccdd2c3c36ac2715216af58bf6731e61eb9ad6
Haider Ali <haider.ali@gjensidige.se>
2025-02-26 17:43:06 +0100

    Fixes to the monitoring script

--- monitor_docker_resources.sh
+++ monitor_docker_resources.sh
@@ -2,37 +2,24 @@
 
 printf "........ Script started .......\n"
 
+chmod +x monitor_docker_resources.sh
 container_name="bigdataanalyticsha-dbstorage-1" # Replace with your container name
 interval=60 # Interval in seconds (1 minute)
 cd /Users/haider.aligjensidige.se/Documents/GitHub/BigDataAnalyticsHA
+docker cp mongo_commands.js bigdataanalyticsha-dbstorage-1:/tmp/mongo_commands.js
 
 #sleep 10 #just taking a breath
 
-# make the shell script executable
-chmod +x monitor_docker_resources.sh
-
 #docker-compose -f all-at-once.yaml stats
 
-printf "\n........ MongoDB Information ........\n"
+printf "\n........ cloneDetector Information ........\n\n"
 
-mongo_output=$(docker exec -it bigdataanalyticsha-dbstorage-1 mongosh \
-  --eval "show dbs" \
-  --eval "use cloneDetector" \
-  --eval "show collections" \
-  --eval "db.chunks.count()"\
-  --eval "db.files.count()"\
-  --eval "db.candidates.count()"\
-  --eval "db.clones.count()")
+while true; do # Run indefinitely
 
-printf "$mongo_output\n"
+  printf "\n........ (Time: $(date +'%Y-%m-%d %H:%M:%S')) ........\n\n"
+  mongo_output=$(docker exec -it bigdataanalyticsha-dbstorage-1 mongosh /tmp/mongo_commands.js)
+  printf "$mongo_output\n"
+
+done
 
 printf  "\n........ Script finished ........\n"
-
-# Copy the JavaScript file into the container
-docker cp mongo_commands.js bigdataanalyticsha-dbstorage-1:/tmp/mongo_commands.js
-
-# Execute the JavaScript file
-mongo_output2=$(docker exec -it bigdataanalyticsha-dbstorage-1 mongosh /tmp/mongo_commands.js)
-
-# Print the output
-printf "$mongo_output2\n"
